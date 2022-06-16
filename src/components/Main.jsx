0ccc9c3a1ef50abaf190809c617d08f0b506a24f
biancapower <bianca@biancapower.com>
2022-06-16 13:42:08 +1000

    add variable, interpolation, and styles

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 
 const Main = () => {
+  const welcome = "Hello there"
+
   return (
-    <h1>Hello World!</h1>
+    <h1 style={{color: 'lime', fontSize: '5em'}}>{welcome}</h1>
   );
 }
 
