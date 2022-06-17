5fed6129cd043a0f5203c6b44087c27d7b1cdd4b
biancapower <bianca@biancapower.com>
2022-06-17 12:06:44 +1000

    refactor avatar link

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 
 const Comment = (props) => {
+  const avatar = "https://i.pravatar.cc/60?u=" + Math.random()
+
   return (
     <div className="comment">
       <a href="/" className="avatar">
-        <img src={props.avatar} alt="avatar" />
+        <img src={avatar} alt="avatar" />
       </a>
       <div className="content">
         <a href="/" className="author">{props.author}</a>
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -12,9 +12,9 @@ const Main = () => {
   return (
     <div>
       <h1 style={styles}>{welcome}</h1>
-      <Comment author="Bob Jones" date="5th June" avatar="https://i.pravatar.cc/60?u=1" text="DummyCorp will transform the aptitude of co-branded versioning to integrate. Our technology takes the best features of Flash and CSS. What does it really mean to syndicate 'magnetically'?" />
-      <Comment author="Tayla Jones" date="6th August" avatar="https://i.pravatar.cc/60?u=2" text="A company that can envisioneer fiercely will (at some indefinite point of time in the future) be able to utilize courageously. Is DummyCorp more important for something to be seamless or to be cross-platform?" />
-      <Comment author="Temperence Jones" date="8th December" avatar="https://i.pravatar.cc/60?u=3" text="Our technology takes the best features of ASP and ActionScript. DummyCorp will optimize the industry jargon 'seamless'. What does the buzzword 'e-services' really mean?" />
+      <Comment author="Bob Jones" date="5th June" text="DummyCorp will transform the aptitude of co-branded versioning to integrate. Our technology takes the best features of Flash and CSS. What does it really mean to syndicate 'magnetically'?" />
+      <Comment author="Tayla Jones" date="6th August" text="A company that can envisioneer fiercely will (at some indefinite point of time in the future) be able to utilize courageously. Is DummyCorp more important for something to be seamless or to be cross-platform?" />
+      <Comment author="Temperence Jones" date="8th December" text="Our technology takes the best features of ASP and ActionScript. DummyCorp will optimize the industry jargon 'seamless'. What does the buzzword 'e-services' really mean?" />
     </div>
   );
 }
