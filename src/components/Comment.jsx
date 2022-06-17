47c0a8cd4276d94cf54ba64a7c0b5fd999d50dbd
biancapower <bianca@biancapower.com>
2022-06-17 20:11:18 +1000

    loop through data

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -2,19 +2,27 @@ import React from "react"
 import { data } from '../data/users'
 
 const Comment = (props) => {
-  // ?u= allows passing a unique identifier to force unique images
-  const avatar = "https://i.pravatar.cc/60?u=" + Math.random()
-
   return (
     <div className="comment">
-      <a href="/" className="avatar">
-        <img src={avatar} alt="avatar" />
-      </a>
-      <div className="content">
-        <a href="/" className="author">{data[0].username}</a> {/* TODO: loop through  */}
-        {/* <p className="date">{data[0].birthdate}</p> */} {/* FIXME: can't use dates here? */}
-        <p className="text">{props.text}</p>
-      </div>
+
+      <h3 className="text">{props.text}</h3>
+
+      {data.map((user, index) => {
+        // console.log(user.birthdate)
+
+        return (
+          <div key={index}>
+            <a href="/" className="avatar">
+              <img src={"https://i.pravatar.cc/60?u=" + Math.random()} alt="avatar" />
+            </a>
+            <div className="content">
+              <a href="/" className="author">{user.username}</a>
+              <p className="date">{user.birthdate.toString()}</p>
+              <p className="password">{user.password}</p>
+            </div>
+          </div>
+        )
+      })}
     </div>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -13,8 +13,6 @@ const Main = () => {
     <div>
       <h1 style={styles}>{welcome}</h1>
       <Comment text="DummyCorp will transform the aptitude of co-branded versioning to integrate. Our technology takes the best features of Flash and CSS. What does it really mean to syndicate 'magnetically'?" />
-      <Comment text="A company that can envisioneer fiercely will (at some indefinite point of time in the future) be able to utilize courageously. Is DummyCorp more important for something to be seamless or to be cross-platform?" />
-      <Comment text="Our technology takes the best features of ASP and ActionScript. DummyCorp will optimize the industry jargon 'seamless'. What does the buzzword 'e-services' really mean?" />
     </div>
   );
 }
