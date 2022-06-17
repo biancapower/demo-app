0a09e1f77ecd85103178951ebd82ec56ec3d0f72
biancapower <bianca@biancapower.com>
2022-06-18 09:00:16 +1000

    define multiple state values and conditionally render component based on what the state value is

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,21 +5,21 @@ class Dashboard extends Component{
 	constructor(props){
 		super(props)
 		// define a state
-		this.state = {latitude: null}
+		this.state = {latitude: null, errorMessage: ''}
 
 		window.navigator.geolocation.getCurrentPosition(
 			position => this.setState({latitude: position.coords.latitude}),
-			error => console.log(error)
+			error => this.setState({errorMessage: error.message})
 		)
 	}
 
 	render (){
-		const {latitude} = this.state
+		const {latitude, errorMessage} = this.state
 
 		return (
 			<div>
 				<h3>{latitude}</h3>
-				<Clock date={new Date()} />
+				{errorMessage || <Clock date={new Date()} />}
 			</div>
 		)
 	}
