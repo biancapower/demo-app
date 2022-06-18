ab454bfd5ee3544b47df9ed2f3cccf22c571f331
biancapower <bianca@biancapower.com>
2022-06-18 16:35:36 +1000

    refactor using lifecycle methods and implement ticking clock

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -5,7 +5,34 @@ class Dashboard extends Component{
 	constructor(props){
 		super(props)
 		// define a state
-		this.state = {latitude: null, errorMessage: '', today: new Date()}
+		this.state = {latitude: null, errorMessage: '', date: new Date()}
+
+		console.log( "1. constructor runs first")
+	}
+
+	isHot(){
+		const {latitude, date} = this.state
+		const month = date.getMonth()
+
+		if(((month > 4 && month <= 9) &&  latitude > 0) || ((month <= 4  || month > 9) && latitude < 0) || latitude === 0){
+			return true
+		}
+		return false
+	}
+
+	getClockIcon(){
+		if(this.isHot()){
+			return "summer.png"
+		}
+		return "winter.png"
+	}
+
+	tick(){
+		this.setState({date: new Date()})
+	}
+
+	componentDidMount(){
+		console.log( "3. componentDidMount runs *after first render*")
 
 		window.navigator.geolocation.getCurrentPosition(
 			position => this.setState({latitude: position.coords.latitude}),
@@ -13,29 +40,29 @@ class Dashboard extends Component{
 		)
 	}
 
-	isNorth(){
-		const {latitude} = this.state
-
-		return latitude > 0
+	componentDidUpdate(prevState){
+		console.log( "4. componentDidUpdate runs *after susequent renders*, not on the first render")
+		if(prevState.date !== this.state.date){
+			this.timerId = setInterval(() =>  this.tick() , 1000)
+		}
 	}
 
-	getClockIcon(){
-		if(this.isNorth()){
-			return "summer.png"
-		}
-		return "winter.png"
+	componentWillUnmount(){
+		console.log( "5. componentWillUnmount")
+		clearInterval(this.timerId)
 	}
 
 	render (){
-		const {latitude, errorMessage, today} = this.state
+		console.log( "2. render runs second")
+		const {latitude, errorMessage, date} = this.state
 		
 		return (
 			<div>
-				<h3>{today.toLocaleString('default', { month: 'long' })}</h3>
+				<h3>{date.toLocaleString('default', { month: 'long' })}</h3>
 				<h4>Latitude: {latitude}</h4>
 				{errorMessage || 
 					<Clock 
-						date={today}
+						date={date}
 						icon={latitude ? this.getClockIcon() : null}
 					/>
 				}
