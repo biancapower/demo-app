16a18c0ee15590409b558951333f7cc1f7c349e7
biancapower <bianca@biancapower.com>
2022-06-18 21:25:42 +1000

    implement form as a controlled component using a functional component and the useState hook

--- src/components/ProjectForm.jsx
+++ src/components/ProjectForm.jsx
@@ -1,12 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 
 const ProjectForm = (props) => {
+	const initialFormState = {
+		name: "",
+		description: ""
+	}
+	const [formData, setFormData] = useState(initialFormState)
+
+	const onFormChange = (event) => {
+		setFormData({
+			...formData,
+			[event.target.name]: event.target.value
+		})
+	}
+
 	return (
 		<form className="form">
 			<label htmlFor="name">Name of project:</label>
-			<input type="text" />
+			<input type="text" name="name" value={formData.name} onChange={onFormChange} />
 			<label htmlFor="description">Description:</label>
-			<input type="text" />
+			<input type="text" name="description" value={formData.description} onChange={onFormChange} />
 			<input type="submit" value="Create new project" />
 		</form>
 	)
